553c0c29813779f5508a63d37525e5fd785076d5
parkinwu <snowy_way@163.com>
2021-02-14 22:18:00 +0800

    init

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(crux benchmark-init monokai-theme dracula-theme emacs-dracula smooth-scroll restart-emacs use-package)))
+   '(hungry-delete hungry-dele crux benchmark-init monokai-theme dracula-theme emacs-dracula smooth-scroll restart-emacs use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -11,7 +11,10 @@
   (setq display-line-numbers-type 'relative)
   (global-display-line-numbers-mode t))
 (use-package crux
-  :bind ("C-c k" . crux-smart-kill-line))
+  :bind ("C-k" . crux-smart-kill-line))
+(use-package hungry-delete
+  :bind (("C-c DEL" . hungry-delete-backward))
+  :bind (("C-c d" . hungry-delete-forward)))
 
 (org-babel-do-load-languages
  'org-babel-load-languages
