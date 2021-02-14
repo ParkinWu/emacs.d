7010bda27f4629fd3a7ca8e994cbe9efff1e01b7
parkinwu <snowy_way@163.com>
2021-02-14 21:26:38 +0800

    init

---
+++ lisp/init-packages.el
@@ -0,0 +1,8 @@
+(use-package restart-emacs)
+(use-package smooth-scroll)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
+
+(provide 'init-packages)
---
+++ lisp/init-startup.el
@@ -0,0 +1,7 @@
+(setq make-backup-files nil
+      auto-save-default nil)
+
+(blink-cursor-mode 1)
+
+(setq inhibit-startup-screen nil)
+(provide 'init-startup)
