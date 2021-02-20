6e0c548d9b8823165a1b22dcb2e84a4e0e99064f
wupengzhan <wupengzhan@kuaishou.com>
2021-02-20 22:11:40 +0800

    修改字号

--- custom.el
+++ custom.el
@@ -10,4 +10,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 120 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 160 :width normal)))))
