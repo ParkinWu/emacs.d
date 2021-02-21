20001fc740f0ce944993f9b9ad927b65de3680e7
parkinwu <snowy_way@163.com>
2021-02-21 19:52:12 +0800

    自定义字体大小

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -2,6 +2,8 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
+(set-frame-font "-ADBO-Source Code Pro-semibold-normal-normal-*-18-*-*-*-m-0-iso10646-1")
+
 (setq redisplay-dont-pause t
   scroll-margin 1
   scroll-step 1
